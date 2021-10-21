c0e39bf3d6ca757c7b57f9da326754fa77bb145c
Bonu93 <luca.morucci.kb@gmail.com>
2021-10-21 14:40:45 +0200

    complete

---
+++ main.js
@@ -0,0 +1,21 @@
+const km = parseInt (prompt('Quanti chilometri devi percorrere?'));
+
+const age = parseInt (prompt('Quanti anni hai?'));
+
+const price = (km * 0.21);
+
+const underDiscount = ((price / 100) * 20)
+
+const overDiscount = ((price / 100) * 40)
+
+let ticket
+
+if (age < 18) {
+    ticket = (price - underDiscount)
+} else if (age >= 65) {
+    ticket = (price - overDiscount)
+} else {
+    ticket = price
+}
+
+document.getElementById('ticket').innerHTML = `Il tuo biglietto costa ${ticket.toFixed(2)}€`
